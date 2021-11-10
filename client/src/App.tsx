508d402213df6602303d5202a33302bb74ac1396
AngelFloresM <angelflores992@gmail.com>
2021-11-10 11:58:27 -0600

    adding test js files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { MouseEvent, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState<number>(0)
+
+  const changeCount = (event: MouseEvent<HTMLButtonElement>) =>{
+    const newCount: number = count + 1
+    setCount(newCount)
+  }
+  
   return (
     <div className="App">
+      <h1>Este es un Header</h1>
+      <p>{count}</p>
+      <button className="addBtn" onClick={changeCount}>Add</button>
     </div>
   );
 }
